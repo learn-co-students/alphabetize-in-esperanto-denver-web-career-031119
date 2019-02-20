9ace2b76815c4bd6aed248cb97988270d2ee5242
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 17:36:15 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(sentence_array)
+  alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  sentence_array.sort_by do |sentence|
+    sentence.split('').map do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
