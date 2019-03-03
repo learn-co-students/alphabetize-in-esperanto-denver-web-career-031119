9e365bb40db2de2b72ba0953dab6d1c3619285b1
github username <github email address>
2019-03-03 15:53:17 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
+
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  alphabetize = arr.sort_by do |word|
+    word.split('').map do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
